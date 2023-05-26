00ee32a81c0d5901734bc63898f3b162621a4c8b
JANUSZ-LAPTOP\Jan <janusz_f@wp.pl>
2023-05-26 23:52:52 +0200

    dodana strona

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,27 +11,30 @@
 		Button
 	} from 'svelte-materialify';
 	import { Icon, NavigationDrawer, Overlay, Menu, ListItem } from 'svelte-materialify';
-	import { mdiMenu } from '@mdi/js';
+	import { mdiBrightness6, mdiMenu, mdiStar } from '@mdi/js';
 	let theme = 'light';
+	let fluid = true;
 
 	function toggleTheme() {
 		if (theme === 'light') theme = 'dark';
 		else theme = 'light';
 	}
-	function toggleTheme11() {
-		if (value < 50) theme = 'dark';
-		else theme = 'light';
-	}
+	// function toggleTheme11() {
+	// 	if (value < 50) theme = 'dark';
+	// 	else theme = 'light';
+	// }
 	let value = 20;
 
 	let active = false;
 	function toggleNavigation() {
 		active = !active;
 	}
+	$: if (value >= 50) theme = 'dark';
+	else theme = 'light';
 </script>
 
 <MaterialApp {theme}>
-	<Container>
+	<Container {fluid}>
 	
 		<AppBar>
 			<div slot="icon">
@@ -41,37 +44,61 @@
 					<Icon path={mdiMenu} />
 				  </Button>
 				</div>
-				<ListItem >Item 1</ListItem>
+				<ListItem ><a href="/oferta">
+					<Button  depressed>
+						
+							<Icon path={mdiStar} class="mr-4" />
+						
+						Nasza Oferta
+					</Button>
+				</a></ListItem>
 				<ListItem >Item 2</ListItem>
 				<ListItem>Item 3</ListItem>
 			  </Menu>
 			</div>
 			<span slot="title">Title</span>
+			<div style="flex-grow:1" />
+  			<Button fab depressed on:click={toggleTheme}>
+					<Icon path={mdiBrightness6} />
+				  </Button>
 		</AppBar>
 		<!-- <NavigationDrawer style="position: relative" {active}>  Content</NavigationDrawer>
 		<Overlay {active} absolute on:click={toggleNavigation} index={1} /> -->
 	
 	
 		<Row>
-			<Col><h3 class="mb-4">Svelte Materialify</h3></Col>
-			<Col>
+			<Col cols={12} sm={12} md={6}><h3 class="mb-4">Svelte Materialify</h3></Col>
+			<Col cols={12} sm={12} md={6}>
 				<br />
-				<Slider thumb persistentThumb bind:value on:change={toggleTheme11} />
+				<Slider thumb persistentThumb bind:value  />
+				<!-- on:change={toggleTheme11} -->
 			</Col>
 		</Row>
-	</Container>
+	
 	<br />
 
 	<Divider />
 
 	<br />
-	<div class="text-center">
-		<a href="//github.com/TheComputerM/svelte-materialify/stargazers">
-			<Button class="primary-color">
+	<Row>
+			<Col cols={12} sm={12} md={6}>
+				<div class="text-center ">
+		<a href="">
+			<Button class="primary-color ">
 				Star on GitHub {value}
 			</Button>
 		</a>
-		<Button on:click={toggleTheme}>Toggle Theme</Button>
-	</div>
-	<br />
+		</div>
+			</Col>
+			<Col cols={12} sm={12} md={6}>
+				<div class="text-center ">
+				<Button on:click={toggleTheme} class="primary-color ">
+					Toggle Theme
+				</Button>
+				</div>
+			</Col>
+		</Row>
+	
+		<br />
+	</Container>
 </MaterialApp>
---
+++ src/routes/oferta/+page.svelte
@@ -0,0 +1,108 @@
+<script>
+    import {
+      Card,
+      CardTitle,
+      CardSubtitle,
+      CardActions,
+      Button,
+      Row,
+      Col,
+      AppBar,
+      Icon,
+    Divider,
+    MaterialApp } from 'svelte-materialify';
+    import { slide } from 'svelte/transition';
+  import { mdiChevronDown , mdiDotsHorizontal} from '@mdi/js';
+  import {  Menu, ListItem } from 'svelte-materialify';
+	import { mdiBrightness6, mdiMenu, mdiStar } from '@mdi/js';
+	let theme = 'light';
+  let active = false;
+	function toggleNavigation() {
+		active = !active;
+	}
+  
+  let active1 = false;
+  function toggle() {
+    active1 = !active1;
+  }
+  function toggleTheme() {
+		if (theme === 'light') theme = 'dark';
+		else theme = 'light';
+	}
+    const songs = [
+      { name: 'Tuba Angel', author: 'Wieczka termoformowane', color: 'blue' },
+      { name: 'Tuba Mech', author: 'Suchy lód', color: 'red' },
+      { name: 'Tuba Archmage', author: 'Rożki wysokocukrowe', color: 'green' },
+      { name: 'List Openheimera', author: 'Owijki', color: 'grey' },
+    ];
+  </script>
+  
+  <MaterialApp {theme}>
+    <AppBar>
+			<div slot="icon">
+			<Menu class="d-flex flex-column">
+				<div slot="activator">
+				  <Button fab depressed on:click={toggleNavigation}>
+					<Icon path={mdiMenu} />
+				  </Button>
+				</div>
+				<ListItem ><a href="/">
+					<Button  depressed>
+						
+							<Icon path={ mdiDotsHorizontal} class="mr-4" />
+						
+						Strona Główna
+					</Button>
+				</a></ListItem>
+				<ListItem >Item 2</ListItem>
+				<ListItem>Item 3</ListItem>
+			  </Menu>
+			</div>
+			<span slot="title">Title</span>
+			<div style="flex-grow:1" />
+  			<Button fab depressed on:click={toggleTheme}>
+					<Icon path={mdiBrightness6} />
+				  </Button>
+		</AppBar>
+
+  <div class="d-flex justify-center mt-4 mb-4">
+    <div class="rounded elevation-2  pa-14 d-flex justify-center flex-wrap" style="flex-grow:1;max-width:800px;gap: 20px;">
+      {#each songs as song, i}
+        <!-- <Card class=" pa-4">
+          <Row>
+            <Col cols={8}>
+              <CardTitle>{song.name}</CardTitle>
+              <CardSubtitle>{song.author}</CardSubtitle>
+              <CardActions>
+                <Button text>Play</Button>
+              </CardActions>
+            </Col>
+            <Col cols={4}><img src="//picsum.photos/100?random={i}" alt="cover" /></Col>
+          </Row>
+        </Card> -->
+        <Card style="max-width:300px;">
+          <img src="//picsum.photos/300?random={i}" alt="background" />
+          <CardTitle>Top western road trips {song.name}</CardTitle>
+          <CardSubtitle>1,000 miles of wonder {song.author}</CardSubtitle>
+          <CardActions>
+            <Button text class={song.color}>Button</Button>
+            <Button text fab size="small" class="ml-auto" on:click={toggle}>
+              <Icon path={mdiChevronDown} rotate={active ? 180 : 0} />
+            </Button>
+          </CardActions>
+          {#if active1}
+            <div transition:slide>
+              <Divider />
+              <div class="pl-4 pr-4 pt-2 pb-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita autem,
+                asperiores dolores, doloremque ea atque suscipit dolore, ut adipisci amet
+                possimus dicta at voluptas consequatur!
+              </div>
+            </div>
+          {/if}
+        </Card>
+        <br />
+      {/each}
+    </div>
+  </div>
+  </MaterialApp>
